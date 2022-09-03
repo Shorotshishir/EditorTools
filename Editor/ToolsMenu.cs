b6e9d9b4185f2bc05f3ab63fa07d11c03330be23
Shorotshishir <tanzim121@gmail.com>
2022-09-03 19:17:29 +0900

    Init Commit

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Directory;
+using static System.IO.Path;
+
+
+namespace siratim
+{
+  public static class ToolsMenu
+  {
+    [MenuItem("Tools/Setup/CreateDefaultFolders")]
+    public static void CreateDefaultFolder()
+    {
+      MakeDir("_Project","Scripts","Materials", "Media/Audio", "Media/Video", "Media/Images");
+      AssetDatabase.Refresh();
+    }
+
+
+    private static void MakeDir(string root, params string[] dirs)
+    {
+      var fullpath = Combine(Application.dataPath, root);
+      foreach (var newDir in dirs) CreateDirectory(Combine(fullpath, newDir));
+    }
+  }
+}
